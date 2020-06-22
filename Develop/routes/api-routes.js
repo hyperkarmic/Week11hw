d133b19cd5ade90a7d475a12abcd490f34b5d0a7
hyperkarma <randy_pan@yahoo.com>
2020-06-22 17:03:16 +0100

    router html page simplified - get functionality added

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,14 +1,10 @@
-const router = require("express").Router(); 
-const store =  require("../db/store");
+const fs = require("fs");
+const path = require("path")
+const noteData = require("../db/db.json")
 
-//* GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-router.get("/notes", (req, res) => {
-store.getAllNotes().then((notes) => res.json(notes))
-.catch((err) => res.status(500).json(err));
-});
+module.exports = function(app){
 
-//* POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-
-//* DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
-module.exports = router; 
+    app.get("/api.notes/", (req,res)=> {
+        res.json(noteData)
+    })
+}
