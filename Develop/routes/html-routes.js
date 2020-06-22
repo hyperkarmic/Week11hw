76b11b5e0245d9270a083b5c579b77ca596c00f9
hyperkarma <randy_pan@yahoo.com>
2020-06-22 15:36:00 +0100

    html routes page changed to a simpler format

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,15 +1,17 @@
+//import dependencies
 const path = require("path");
-const router = require("express").Router();
 
-//* GET `/notes` - Should return the `notes.html` file.
-  router.get("/notes", (req, res) => {
-      res.sendFile(path.join(__dirname, "../public/notes.html"));
-  });
+//routing 
+module.exports = (app) => {
 
- // * GET `*` - Should return the `index.html` file
- router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+    //the two HTML get routes
+
+    app.get("/notes/", (req,res) => {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+    app.get("/",(req,res) => {
+     res.sendFile(path.join(__dirname, "../public/index.html"));   
+    });
+};
 
 
- module.exports = router;
