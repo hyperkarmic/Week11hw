91e362143c6fbbab8cfd209e43726f6375581da0
hyperkarma <randy_pan@yahoo.com>
2020-06-26 13:00:48 +0100

    routes file removed.  server file has promisify, and app.set added to middlewhere.  Routes added in server file.  Route functions templated

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,81 @@
+//grabbing dependency
 const express = require("express");
+
+//initializing instance
 const app = express();
-const apiRoutes = require("./routes/api-routes");
-const htmlRoutes = require("./routes/html-routes");
 
 const PORT = process.env.PORT || 3000;
 
+//middle-ware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true})); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.set({
+  "Access-Control-Allow-Origin": "*",
+  "X-Content-Type-Options": "nosniff",
+});
+
+//promisify read/write file variables initialized
+const readFileAsync = promisify(fs.readfile)
+const writeFileAsync = promisify(fs.writefile)
+
+//this is where the functions will be populated
+
+const serveHome = (req, res) => {
+    
+  };
+
+  const serveNotes = (req, res) => {
+    
+};
+
+const getJS = (req, res) => {
+    
+};
+
+const getCSS = (req, res) => {
+    
+};
+
+const getNotes = async (req, res) => {
+    
+};
+
+const postNotes = async (req, res) => {
+    
+};
+
+const delNotes = async (req, res) => {
+    
+};
+
+
+
+
+
+
+
+
+
+//the routes are to be populated underneath
+
+
+app.get("/", serveHome)
+app.get("/notes", serveNotes)
+//these constitute the html routes
+
+app.get("/public", getJS)
+app.get("/public", getCSS)
+//these allow the accessing of necessary CSS/JS for the index page
+
+//api routes for read/write/delete functionality
+
+app.get("/api/notes",getNotes)
+app.post("/api/notes",postNotes)
+app.delete("/api/notes:id",delNotes)
+
+
+
+
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
