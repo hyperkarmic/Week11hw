0d970bddaad791a2ab24494e0b4a5f035cfbc451
hyperkarma <randy_pan@yahoo.com>
2020-06-28 22:50:07 +0100

    commenting completed, readme file added

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,4 @@
- //grabbing dependency
+//grabbing dependency
 const express = require("express");
 const fs = require("fs");
 const { promisify } = require("util");
@@ -52,38 +52,45 @@ const getNotes = async (req, res) => {
   res.json(notes);
 };
 
+//this is the functionality for saving notes
 const postNotes = async (req, res) => {
   const newNotes = req.body;
   const filePath = path.join(__dirname, "/db/db.json");
   const parsedNotesData = await grabNotesData();
 
   const genId = parsedNotesData.length + 1;
+  //this gives us our id
 
   parsedNotesData.push({
     id: genId,
     title: newNotes.title,
     text: newNotes.text,
   });
+  //this adds latest note, to the note array
 
   await noteWriter(parsedNotesData);
 
   res.status(200).sendFile(filePath);
 };
 
+//this function provides delete functionality
+
 const delNotes = async (req, res) => {
-  console.log("i fired");
   const filePath = path.join(__dirname, "/public/notes.html");
   const dataBaseFilePath = path.join(__dirname, "/db/db.json");
-  const notesData = await readFileAsync(dataBaseFilePath, "utf-8");
+  const notesData = await readFileAsync(dataBaseFilePath, "UTF8");
   const noteParse = JSON.parse(notesData);
-  console.log(noteParse);
 
-  noteParse.splice(noteParse[req.params.id - 1], 1);
+  noteParse.splice(noteParse[req.params.id], 1);
+  //the note with a specific id is removed, using a splice function with one item of
+  //to remove.  I was going to use a filter function but this seems to work.
+
   console.log(noteParse);
   await writeFileAsync(dataBaseFilePath, JSON.stringify(noteParse), "UTF8");
   res.sendFile(filePath);
 };
 
+//this function accesses and parses note data
 const grabNotesData = async () => {
   const notesFilePath = path.join(__dirname, "/db/db.json");
   const notesData = await readFileAsync(notesFilePath, "UTF8");
@@ -91,6 +98,7 @@ const grabNotesData = async () => {
   return notesParsed;
 };
 
+//this function writes notes to the database file
 const noteWriter = async (parsedNotesData) => {
   const notesFilePath = path.join(__dirname, "/db/db.json");
   await writeFileAsync(notesFilePath, JSON.stringify(parsedNotesData));
@@ -110,6 +118,7 @@ app.get("/public", getCSS);
 
 app.get("/api/notes", getNotes);
 app.post("/api/notes", postNotes);
-app.delete("/api/notes:id", delNotes);
+app.delete("/api/notes/:id", delNotes);
 
+//this supplies port listening functionality!
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
